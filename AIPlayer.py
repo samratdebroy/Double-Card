f939688130e1d7b7b6a1b0284cd91497b56433e5
Simon Bourque <7109056+simon-bourque@users.noreply.github.com>
2019-03-04 15:40:29 -0500

    Add abstract Player class

---
+++ AIPlayer.py
@@ -0,0 +1,9 @@
+from Player import Player
+
+class AIPlayer(Player):
+
+    def __init__(self, winning_token):
+        Player.__init__(self, winning_token)
+
+    def play_turn(self):
+        pass
---
+++ Player.py
@@ -0,0 +1,14 @@
+import abc
+
+class Player(abc.ABC):
+    # Winning token
+    COLOR_WIN = 0
+    DOT_WIN = 1
+
+    def __init__(self, winning_token):
+        self.winning_token = winning_token
+
+    # Override in derived class
+    @abc.abstractmethod
+    def play_turn(self):
+        pass
